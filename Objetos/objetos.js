61613e96ffaf6dec438fa6cabb40a98495289249
EnocPAguirre <enocpinedaa@gmail.cmo>
2022-01-01 20:32:40 -0600

    Continuando con el curso

--- Objetos/objetos.js
+++ Objetos/objetos.js
@@ -5,8 +5,31 @@ const producto = {
 };
 
 // Agregando nuevas propiedades 
-
 producto.imagen = 'imagen.png';
+//Eliminar propiedades de un objeto
 delete producto.dispobible;
 
 console.log(producto)
+
+// Destructuracion
+const {nombre, precio} = producto;
+
+console.log(nombre)
+console.log(precio)
+
+// Crear objeto dentro de objeto
+producto.informacion = {
+    medidas: {
+        peso: "1KG",
+        medida: "1m"
+    }
+}
+
+console.log(producto)
+
+// Descructuracion de un objeto dentro de otro objeto
+
+const { informacion: { medidas}} = producto;
+
+console.log(medidas)
+
